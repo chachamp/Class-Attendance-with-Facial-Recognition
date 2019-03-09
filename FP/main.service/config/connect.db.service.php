f37f6c117a22388eb07b20f4d1c564539a288046
chachamp <48378943+chachamp@users.noreply.github.com>
2019-03-09 18:35:01 +0700

    Add files via upload

---
+++ FP/main.service/config/connect.db.service.php
@@ -0,0 +1,19 @@
+<?php
+define('servername','localhost');
+define('username','root');
+define('password','kokkak1');
+define('dbname','db_main');
+
+$objCon = mysqli_connect("35.194.252.40:3306","hahahaha","12345678","db_main");
+
+// Check for Connecting 
+
+// if($objCon){
+// echo "Database Connected.";
+// }
+// else{
+// echo "Database Connect Failed.";
+// }
+mysqli_set_charset($objCon, "utf8")
+
+?>
